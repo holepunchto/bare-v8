6ed44aa1733713ca43211001565fea5fdd6bae37
Yasser Nascimento <dev@yasser.email>
2025-10-01 12:47:47 -0300

    Add `getHeapSpaceStatistics`

--- test.js
+++ test.js
@@ -4,3 +4,7 @@ const v8 = require('.')
 test('heap statistics', (t) => {
   t.comment(v8.getHeapStatistics())
 })
+
+test('heap space statistics', (t) => {
+  t.comment(v8.getHeapSpaceStatistics())
+})
